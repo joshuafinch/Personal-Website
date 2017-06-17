e5528f792996c0a6af241ccb064e4faa438387bd
Joshua Finch <josh@joshuafinch.com>
2017-06-17 23:27:32 +0100

    Use rsync directly

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Starting rsync"
-rsync -r --exclude '/.htaccess' --exclude '/.well-known/' --delete-after --quiet $TRAVIS_BUILD_DIR/public $DEPLOYUSER@$DEPLOYHOST:$DEPLOYDIR
-echo "Finished rsync"
