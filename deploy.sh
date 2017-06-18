92efc4887d29010deb704906436e91cb0090dc7a
Joshua Finch <josh@joshuafinch.com>
2017-06-18 14:56:58 +0100

    Ping sitemaps on code changes

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,10 @@ echo "Purge the cloudflare cache"
 # Purge the cloudflare cache
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLAREZONEID}/purge_cache" -H "X-Auth-Email: ${CLOUDFLAREAUTHEMAIL}" -H "X-Auth-Key: ${CLOUDFLAREAUTHKEY}" -H "Content-Type: application/json" --data '{"purge_everything":true}'
 
+echo "Ping the sitemap to google"
+curl http://google.com/ping?sitemap=https://joshua.codes/sitemap.xml
+
+echo "Ping the sitemap to bing"
+curl http://www.bing.com/webmaster/ping.aspx?siteMap=https://joshua.codes/sitemap.xml
+
 echo "Finished deploy.sh"
